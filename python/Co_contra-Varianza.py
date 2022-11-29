ae013a54d09a25a31ed25914ed4fe86bbd3964b2
PaulLuqueCcosi <pluquec@unsa.edu.pe>
2022-11-28 19:07:19 -0500

    Agregando jerarquia inicial

---
+++ python/Co_contra-Varianza.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    @abstractmethod
+    def f_a(self):
+        pass
+class B(A):
+    @abstractmethod
+    def f_b(self):
+        pass
+
+class C(B):
+    @abstractmethod
+    def f_c(self):
+        print("f_c")
+
+class A_impl(A):
+    def f_a(self):
+        print("A_impl.f_a")
+
+
+class B_impl(B):
+    def f_b(self):
+        print("B_impl.f_b")
+    
+    def f_a(self):
+        print("B_impl.f_a")
+
+class C_impl(C):
+    def f_c(self):
+        print("C_impl.f_c")
+
+    def f_b(self):
+        print("C_impl.f_b")
+    
+    def f_a(self):
+        print("C_impl.f_a")
+
+
+
+class X:
+    def __init__(self):
+        pass
+
+    def foo(self, b):
+        b.f_b()
+        return b
+
+class Y(X):
+    def __init__(self):
+        pass
+    
+    def foo(self, b):                                                                                           
+        b.f_a()
+        b.f_b()
+        return b
+
+if __name__ == "__main__":
+    a = A_impl()
+    b = B_impl()
+    c = C_impl()
+    y = Y()
+    y.foo(b)
